4671c7eefe16236edc85957c88fd4f312d71faaa
Julie Loi <36089473+JLoiLeaf@users.noreply.github.com>
2019-11-11 11:31:00 -0800

    Add files via upload

---
+++ framework/Command.java
@@ -0,0 +1,10 @@
+package framework;
+
+public abstract class Command {
+	
+	// Variable(s)
+	protected Model model;
+	
+	public abstract void execute();
+	
+}
---
+++ framework/CommandProcessor.java
@@ -0,0 +1,10 @@
+package framework;
+
+public class CommandProcessor {
+	
+	// Executes the command
+	public static void execute(Command cmmd) { 
+		cmmd.execute(); 
+	}
+	
+}
