94806dd28e209b8eb51a714caf540d3f89504434
Julie Loi <36089473+JLoiLeaf@users.noreply.github.com>
2019-11-11 11:46:51 -0800

    Add files via upload

---
+++ presentation/MazeFactory.java
@@ -0,0 +1,93 @@
+package presentation;
+
+import java.awt.event.ActionListener;
+
+import business.*;
+import framework.*;
+
+public class MazeFactory implements AppFactory {
+	
+	@Override
+	public Model makeModel() {
+		return new Maze();
+	}
+
+	
+	
+	// NEEDS MAZEPANEL
+	@Override
+	public AppPanel makePanel(Model model, ActionListener listener) {
+		return new MazePanel(model, listener);		//FINISH ADDING PARAMETERS
+	}
+	//NEEDS MAZEPANEL
+	
+	
+
+	@Override
+	public String[] getEditCommands() {
+		// TODO Auto-generated method stub
+		return new String[] { "North", "South", "East", "West", "Reset" };
+	}
+
+	
+	
+	//UNFINISHED: NEED COMMAND CLASSES
+	@Override
+	public Command makeEditCommand(Model model, String type) {
+		
+		if (type == "North") {
+			
+		//	return new SetNorth(model); 		// These are the commands
+			
+		}
+		else if (type == "South") {
+		
+		//	return new SetNorth(model); 		// These are the commands	
+			
+		}
+		else if (type == "East") {
+			
+		//	return new SetNorth(model); 		// These are the commands
+			
+		}
+		else if (type == "West") {
+			
+			//	return new SetNorth(model); 		// These are the commands
+
+		}
+		else if (type == "Reset") {
+			
+			//	return new SetNorth(model); 		// These are the commands
+
+		}
+	
+		
+		
+		return null;
+	}
+	// UNFINISHED
+	
+	
+	
+	
+
+	@Override
+	public String getTitle() {
+		String title = "Maze Challenge V1.0";
+		return title;
+	}
+
+	@Override
+	public String[] getHelp() {
+		String[] help = new String[1];
+		help[1] = "Locate the escape room in the number of allocated moves";
+		return help;
+	}
+
+	@Override
+	public String about() {
+		String about = "Maze Challenge V1.0 by Lynx 2019";
+		return about;
+	}
+
+}
---
+++ presentation/MazePanel.java
@@ -0,0 +1,14 @@
+package presentation;
+
+import java.awt.event.ActionListener;
+
+import framework.*;
+
+public class MazePanel extends AppPanel{
+
+	public MazePanel(Model model, ActionListener listener) {
+		// TODO Auto-generated constructor stub
+		super(model, listener);
+	}
+
+}
